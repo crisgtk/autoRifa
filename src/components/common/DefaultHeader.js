2868cbb5b33f2e172fcfebc4a992d74fbe8a3734
crisgtk <63614285+crisgtk@users.noreply.github.com>
2025-08-03 22:41:25 -0400

    add header and promotion (#5)
    Co-authored-by: Cristian Hernan Burgos Salgado <cristian.burgos+meli@mercadolibre.cl>

--- src/components/common/DefaultHeader.js
+++ src/components/common/DefaultHeader.js
@@ -1,8 +1,6 @@
 "use client";
 
 import MainMenu from "@/components/common/MainMenu";
-import SidebarPanel from "@/components/common/sidebar-panel";
-import LoginSignupModal from "@/components/common/login-signup-modal";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -40,17 +38,17 @@ const DefaultHeader = () => {
                   <div className="logos mr40">
                     <Link className="header-logo logo1" href="/">
                       <Image
-                        width={138}
-                        height={44}
-                        src="/images/header-logo2.svg"
+                        width={220}
+                        height={80}
+                        src="/images/ai1.png"
                         alt="Header Logo"
                       />
                     </Link>
                     <Link className="header-logo logo2" href="/">
                       <Image
-                        width={138}
-                        height={44}
-                        src="/images/header-logo2.svg"
+                        width={200}
+                        height={70}
+                        src="/images/ai1.png"
                         alt="Header Logo"
                       />
                     </Link>
@@ -66,44 +64,19 @@ const DefaultHeader = () => {
               <div className="col-auto">
                 <div className="d-flex align-items-center">
                   <a
-                    href="#"
-                    className="login-info d-flex align-items-cente"
-                    data-bs-toggle="modal"
-                    data-bs-target="#loginSignupModal"
-                    role="button"
+                    className="login-info d-flex align-items-center me-3"
+                    href="tel:+012305094502"
                   >
-                    <i className="far fa-user-circle fz16 me-2" />{" "}
-                    <span className="d-none d-xl-block">Login / Register</span>
+                    <i className="far fa-phone fz16 me-2"></i>{" "}
+                    <span className="d-none d-xl-block">+56 989957576</span>
                   </a>
                   <Link
-                    className="ud-btn btn-white add-property bdrs60 mx-2 mx-xl-4"
-                    href="/dashboard-add-property"
+                    className="ud-btn add-property menu-btn bdrs60 mx-2 mx-xl-4"
+                    href="/ticket"
                   >
-                    Add Property
+                    Compra tu ticket
                     <i className="fal fa-arrow-right-long" />
                   </Link>
-                  <a
-                    className="sidemenu-btn filter-btn-right"
-                    href="#"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#SidebarPanel"
-                    aria-controls="SidebarPanelLabel"
-                  >
-                    <Image
-                      width={25}
-                      height={9}
-                      className="img-1"
-                      src="/images/dark-nav-icon.svg"
-                      alt="humberger menu"
-                    />
-                    <Image
-                      width={25}
-                      height={9}
-                      className="img-2"
-                      src="/images/dark-nav-icon.svg"
-                      alt="humberger menu"
-                    />
-                  </a>
                 </div>
               </div>
               {/* End .col-auto */}
@@ -114,31 +87,17 @@ const DefaultHeader = () => {
       </header>
       {/* End Header */}
 
-      {/* Signup Modal */}
-      <div className="signup-modal">
-        <div
-          className="modal fade"
-          id="loginSignupModal"
-          tabIndex={-1}
-          aria-labelledby="loginSignupModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog  modal-dialog-scrollable modal-dialog-centered">
-            <LoginSignupModal />
-          </div>
-        </div>
-      </div>
-      {/* End Signup Modal */}
+
 
       {/* DesktopSidebarMenu */}
-      <div
+      {/* <div
         className="offcanvas offcanvas-end"
         tabIndex="-1"
         id="SidebarPanel"
         aria-labelledby="SidebarPanelLabel"
-      >
-        <SidebarPanel />
-      </div>
+      > */}
+        {/* <SidebarPanel /> */}
+      {/* </div> */}
       {/* Sidebar Panel End */}
     </>
   );
